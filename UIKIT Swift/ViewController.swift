ef82254194c95a65ee65166a4267e0e6c70cd279
pedrotura <phgt132@gmail.com>
2025-05-12 21:00:03 -0300

    Finalizando aplicativo

--- UIKIT Swift/ViewController.swift
+++ UIKIT Swift/ViewController.swift
@@ -15,8 +15,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var labelPAD: UILabel!
     @IBOutlet weak var labelLink: UILabel!
     
-    @IBOutlet weak var stpIdade: UIStepper!
-    
     @IBOutlet weak var segRisco: UISegmentedControl!
     
     override func viewDidLoad() {
@@ -24,8 +22,76 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func mudarValorStepper(_ sender: Any) {
-        labelIdade.text = "\(Int(stpIdade.value))"
+    @IBAction func mudarStepper(_ sender: UIStepper) {
+        labelIdade.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func mudarStepperPAS(_ sender: UISlider) {
+        labelPAS.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func mudarStepperPAD(_ sender: UISlider) {
+        labelPAD.text = "\(Int(sender.value))"
+    }
+    
+    @IBAction func mudarSwitch(_ sender: UISwitch) {
+        print(NSNumber(value: sender.isOn))
+    }
+    
+    @IBAction func receberLink(_ sender: Any) {
+        let alerta = UIAlertController(
+            title: "Forma para receber o exame",
+            message: "Faça sua escolha",
+            preferredStyle: UIAlertController.Style.actionSheet
+        )
+        
+        alerta.addAction(UIAlertAction(
+            title: "SMS",
+            style: UIAlertAction.Style.default,
+            handler: {action in
+                self.labelLink.text = action.title
+            }))
+        
+        alerta.addAction(UIAlertAction(
+            title: "E-mail",
+            style: UIAlertAction.Style.default,
+            handler: {action in
+                self.labelLink.text = action.title
+            }))
+        
+        alerta.addAction(UIAlertAction(
+            title: "Cancelar",
+            style: UIAlertAction.Style.cancel,
+            handler: nil
+        ))
+        
+        present(alerta, animated: true)
+    }
+    
+    @IBAction func avancar(_ sender: Any) {
+        var tipo: String
+        var msg: String
+        
+        if (segRisco.selectedSegmentIndex >= 0) {
+            tipo = segRisco.titleForSegment(at: segRisco.selectedSegmentIndex)!
+            msg = "Triagem \(tipo). Paciente com idade de \(labelIdade.text!) salvo com sucesso!"
+        } else {
+            tipo = ""
+            msg = "Antes de salvar, escolha a classificação de risco"
+        }
+        
+        let alerta = UIAlertController(
+            title: "Atenção",
+            message: msg,
+            preferredStyle: UIAlertController.Style.alert
+        )
+        
+        alerta.addAction(UIAlertAction(
+            title: "OK",
+            style: UIAlertAction.Style.default
+        ))
+        
+        present(alerta, animated: true)
     }
     
     @IBAction func mudarIndice(_ sender: Any) {
